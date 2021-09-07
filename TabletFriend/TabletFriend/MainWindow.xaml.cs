1713a09ef38ec1ddbddcad9708b3d98085406337
Martenfur <patych7@gmail.com>
2021-09-08 00:18:48 +0200

    Additional window setup.

--- TabletFriend/TabletFriend/MainWindow.xaml.cs
+++ TabletFriend/TabletFriend/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace TabletFriend
 {
@@ -22,7 +13,43 @@ namespace TabletFriend
 	{
 		public MainWindow()
 		{
+			Topmost = true;
 			InitializeComponent();
+			MouseDown += OnMouseDown;
+		}
+
+		private void OnMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				DragMove();
+			}
+		}
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+
+			// Makes it so the window doesn't hold focus.
+			var helper = new WindowInteropHelper(this);
+			SetWindowLong(
+				helper.Handle, 
+				GWL_EXSTYLE,
+				GetWindowLong(helper.Handle, GWL_EXSTYLE) | WS_EX_NOACTIVATE
+			);
+		}
+
+		private const int GWL_EXSTYLE = -20;
+		private const int WS_EX_NOACTIVATE = 0x08000000;
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+		[DllImport("user32.dll")]
+		public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+		private void cock_Click(object sender, RoutedEventArgs e)
+		{
 		}
 	}
 }
