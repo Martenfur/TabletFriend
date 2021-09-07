5791260e93ee6e86b2b0c8a10346a5137fef3950
Martenfur <patych7@gmail.com>
2021-09-08 00:12:40 +0200

    Fixed indents and made the window transparent.

--- TabletFriend/TabletFriend/MainWindow.xaml.cs
+++ TabletFriend/TabletFriend/MainWindow.xaml.cs
@@ -15,14 +15,14 @@ using System.Windows.Shapes;
 
 namespace TabletFriend
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
 }
