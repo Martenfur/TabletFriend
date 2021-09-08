26b3f84b2cca5bdeee033a62c53a32060d255946
Martenfur <patych7@gmail.com>
2021-09-08 02:21:58 +0200

    Added test yaml conversion.

---
+++ TabletFriend/TabletFriend/Data/ButtonData.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TabletFriend.Data
+{
+	public class ButtonData
+	{
+		public string Action;
+		public string[] Actions;
+		public string Text = "";
+		public string Icon;
+		public string Size = "1,2";
+	}
+}
---
+++ TabletFriend/TabletFriend/Models/ButtonModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using TabletFriend.Data;
+
+namespace TabletFriend.Models
+{
+	public class ButtonModel
+	{
+		public string[] Actions;
+		
+		public string Text;
+		public string Icon;
+
+		public Vector2 Position = Vector2.Zero;
+		public Vector2 Size = Vector2.One;
+
+		public ButtonModel(ButtonData data)
+		{
+			Text = data.Text ?? "";
+			Icon = data.Icon;
+			
+			if (data.Size != null)
+			{
+				Size = ModelConverter.ConvertVector2(data.Size);
+			}
+		}
+	}
+}
