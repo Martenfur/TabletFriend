1bb45a7ca6b59e57e7163a5e2cde3647373e9d82
Martenfur <patych7@gmail.com>
2021-09-24 00:24:37 +0200

    Reverted file watcher.

--- TabletFriend/TabletFriend/FileManager.cs
+++ TabletFriend/TabletFriend/FileManager.cs
@@ -1,48 +1,53 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Timers;
 
 namespace TabletFriend
 {
 	public class FileManager
 	{
-		private SoftMade.IO.AdvancedFileSystemWatcher _watcher;
+		private FileSystemWatcher _watcher;
 
 		public FileManager()
 		{
-			_watcher = new SoftMade.IO.AdvancedFileSystemWatcher();
+			_watcher = new FileSystemWatcher();
 			_watcher.Path = AppState.FilesRoot;
-			_watcher.NotifyFilter = NotifyFilters.LastWrite
+			_watcher.NotifyFilter = NotifyFilters.FileName 
+				| NotifyFilters.DirectoryName
+				| NotifyFilters.Attributes
 				| NotifyFilters.Size
 				| NotifyFilters.LastWrite
+				| NotifyFilters.LastAccess
 				| NotifyFilters.CreationTime
-				| NotifyFilters.FileName
-				| NotifyFilters.DirectoryName;
-			
-			_watcher.Changed += InternalOnChanged;
-			_watcher.Created += InternalOnChanged;
-			_watcher.Deleted += InternalOnChanged;
+				| NotifyFilters.Security;
+
+
+			_watcher.Changed += OnChanged;
+			_watcher.Created += OnChanged;
+			_watcher.Deleted += OnChanged;
 			_watcher.EnableRaisingEvents = true;
 			_watcher.IncludeSubdirectories = true;
 
 			RefreshLayoutList();
 		}
 
-
-		private void InternalOnChanged(object sender, SoftMade.IO.FileSystemEventArgs args)
+		private void OnElapsed(object sender, ElapsedEventArgs e)
 		{
-			if (args.ChangeType != SoftMade.IO.WatcherChangeTypes.BeginWrite)
-			{
-				RefreshLayoutList();
-				EventBeacon.SendEvent("files_changed", sender, args);
-			}
+			Debug.WriteLine("UNBLOCKED");
+		}
+
+		private void OnChanged(object sender, FileSystemEventArgs args)
+		{
+			RefreshLayoutList();
+			EventBeacon.SendEvent("files_changed", sender, args);
 		}
 
 
-		public void RefreshLayoutList()
+		private void RefreshLayoutList()
 		{
-			EventBeacon.SendEvent("update_layout_list");
 			AppState.Layouts = Directory.GetFiles(AppState.LayoutRoot, AppState.LayoutExtension);
+			EventBeacon.SendEvent("update_layout_list");
 		}
-
 	}
 }
