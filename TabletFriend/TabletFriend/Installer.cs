be33988346c92d892434f36a0f79e814c42cc6c5
Martenfur <patych7@gmail.com>
2021-10-01 00:39:58 +0200

    Made the installer support updating.

--- TabletFriend/TabletFriend/Installer.cs
+++ TabletFriend/TabletFriend/Installer.cs
@@ -29,14 +29,45 @@ namespace TabletFriend
 			);
 			if (result == MessageBoxResult.Yes)
 			{
-				DirectoryCopy(AppState.CurrentDirectory, _preferredDirectory);
-				Process.Start(Path.Combine(_preferredDirectory, "TabletFriend.exe"));
-				Application.Current.Shutdown();
+				try
+				{
+					if (Directory.Exists(_preferredDirectory))
+					{
+						MessageBox.Show(
+							"Another version of Tablet Friend detected. 'files' directory will be overwritten. " +
+							"Previous version's layouts, themes and icons will be moved to 'files.backup'",
+							"Update",
+							MessageBoxButton.OK
+						);
+						if (Directory.Exists(Path.Combine(_preferredDirectory, "files")))
+						{
+							DirectoryCopy(
+								Path.Combine(_preferredDirectory, "files"),
+								Path.Combine(_preferredDirectory, "files.backup")
+							);
+						}
+					}
+					DirectoryCopy(AppState.CurrentDirectory, _preferredDirectory, "*.dll");
+					DirectoryCopy(AppState.CurrentDirectory, _preferredDirectory, "*.exe");
+					DirectoryCopy(AppState.CurrentDirectory, _preferredDirectory, "*.json");
+					Process.Start(Path.Combine(_preferredDirectory, "TabletFriend.exe"));
+					Application.Current.Shutdown();
+				}
+				catch (Exception e)
+				{
+					MessageBox.Show(
+						"Failed to copy the files: '" + e.Message + "'. Make sure all other instances of Tablet Friend are closed and try again.",
+						"Error!",
+						MessageBoxButton.OK,
+						MessageBoxImage.Error
+					);
+					Environment.Exit(0);
+				}
 			}
 		}
 
 
-		private static void DirectoryCopy(string sourceDirName, string destDirName)
+		private static void DirectoryCopy(string sourceDirName, string destDirName, string pattern = "*.*")
 		{
 			// Get the subdirectories for the specified directory.
 			DirectoryInfo dir = new DirectoryInfo(sourceDirName);
@@ -54,7 +85,7 @@ namespace TabletFriend
 			Directory.CreateDirectory(destDirName);
 
 			// Get the files in the directory and copy them to the new location.
-			FileInfo[] files = dir.GetFiles();
+			FileInfo[] files = dir.GetFiles(pattern);
 			foreach (FileInfo file in files)
 			{
 				string tempPath = Path.Combine(destDirName, file.Name);
